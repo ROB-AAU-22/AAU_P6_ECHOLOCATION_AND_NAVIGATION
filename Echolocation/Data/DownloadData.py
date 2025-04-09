e0358f98080d16872db84e855277e3e96ac581b2
Villiam Østergaard Brøkner <74359785+Vollenator@users.noreply.github.com>
2025-04-09 12:25:36 +0200

    Update DownloadData.py

--- Echolocation/Data/DownloadData.py
+++ Echolocation/Data/DownloadData.py
@@ -1,7 +1,8 @@
 import torch
-from torchvision.datasets.utils import download_file_from_google_drive, _extract_zip
+from torchvision.datasets.utils import download_file_from_google_drive
 import zipfile
 
+#pip install gdown
 DRIVE_FOLDER_LINK = "1oRUs8O4nU3b2e6uLXILHdhJwXXxJm91R"
 
 def download_data(save_path, file_name):
@@ -9,10 +10,10 @@ def download_data(save_path, file_name):
     return
 
 if __name__ == "__main__":
-    download_data("dataset_zip", "dataset.zip")
+    download_data("Echolocation/Data/dataset_zip", "dataset.zip")
     # extract
-    with zipfile.ZipFile("dataset_zip/dataset.zip", 'r') as zip_ref:
-        zip_ref.extractall("dataset")
+    with zipfile.ZipFile("Echolocation/Data/dataset_zip/dataset.zip", 'r') as zip_ref:
+        zip_ref.extractall("Echolocation/Data/dataset")
 
 
 
