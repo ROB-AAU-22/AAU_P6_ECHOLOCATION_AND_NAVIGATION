eedf3ea68894f43893bf1a2ce216934d70b88790
frederikhp8999 <74359735+frederikhp8999@users.noreply.github.com>
2025-05-06 10:15:37 +0200

    Refactor main.py to comment out model loading and prediction code

--- Echolocation/MachineLearning/Training/mainTraining.py
+++ Echolocation/MachineLearning/Training/mainTraining.py
@@ -10,17 +10,10 @@ import torch.optim as optim
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
 from MachineLearning.Training.TrainingConfig import LEARNING_RATES, HIDDEN_SIZES, BATCH_SIZES, NUM_EPOCHS, \
-<<<<<<< HEAD
-    NUM_LAYERS_LIST, CLASSIFICATION_THRESHOLDS, DISTANCE_THRESHOLD
-from MachineLearning.Training.DataHandler import build_dataset_from_csv
-from MachineLearning.Training.ModelFunctions import MaskedMSELoss, AudioLidarDataset, MLPRegressor, Regressor, Classifier
-from MachineLearning.Training.ModelTraining import compute_error_metrics, train_regressor, evaluate_regressor, ClassifierDataset, train_classifier, evaluate_classifier
-=======
     NUM_LAYERS_LIST, CLASSIFICATION_THRESHOLDS, DISTANCE_THRESHOLD, DISTANCE_THRESHOLDS
 from MachineLearning.Training.DataHandler import build_dataset_from_csv, ClassifierDataset
 from MachineLearning.Training.ModelFunctions import MaskedMSELoss, AudioLidarDataset, MLPRegressor, Regressor, Classifier
 from MachineLearning.Training.ModelTraining import compute_error_metrics, train_regressor, evaluate_regressor, train_classifier, evaluate_classifier
->>>>>>> 33b33186c4a7ba3f4386c6877c2b8c7ffe8be30f
 from MachineLearning.Training.Plotting import start_multiprocessing_plotting
 from MachineLearning.Training.EvaluationPlots import plot_precision_recall_curve, plot_confusion_matrix_all, plot_roc_curve
 
